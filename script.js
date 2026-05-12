0469bc973224a65951fa90b0147d0d741696db85
Benaun <andrey.kuhtin126@gmail.com>
2026-05-12 20:42:47 +0300

    add function to fill cells and styles for filled cells

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 import { Sudoku } from "./sudoku.js";
+import { convertIndexToPosition, GIRD_SIZE } from "./utilities.js";
 
 const sudoku = new Sudoku();
+let cells;
+
+const init = () => {
+  initCells();
+};
+
+const initCells = () => {
+  cells = document.querySelectorAll(".cell");
+  fillCells();
+};
+
+const fillCells = () => {
+  for (let i = 0; i < GIRD_SIZE * GIRD_SIZE; i++) {
+    const { row, column } = convertIndexToPosition(i);
+
+    if (sudoku.grid[row][column] !== null) {
+      cells[i].classList.add("filled");
+      cells[i].innerHTML = sudoku.grid[row][column];
+    }
+  }
+};
+
+init();
--- utilities.js
+++ utilities.js
@@ -1,2 +1,9 @@
 export const GIRD_SIZE = 9;
 export const BOX_SIZE = 3;
+
+export const convertIndexToPosition = (index) => {
+  return {
+    row: Math.floor(index / GIRD_SIZE),
+    column: index % GIRD_SIZE,
+  };
+};
